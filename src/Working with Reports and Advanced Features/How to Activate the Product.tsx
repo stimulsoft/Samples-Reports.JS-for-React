33ba4acd12c98e9004703c08f09c9aad2e1cd5af
Serge Puhov <50665755+MuseMuses@users.noreply.github.com>
2026-06-19 11:06:03 +0300

    Some Improvements
    https://github.com/stimulsoft/Stimulsoft.Reports/issues/20507

--- src/Working with Reports and Advanced Features/How to Activate the Product.tsx
+++ src/Working with Reports and Advanced Features/How to Activate the Product.tsx
@@ -5,7 +5,7 @@ function App() {
     // Using one of the functions below to register your license from the JavaScript code
 
     // Stimulsoft.Base.StiLicense.key = '6vJhGtLLLz2GNviWmUTrhSqnO...';
-    // Stimulsoft.Base.StiLicense.loadFromFile('license.key')
+    // Stimulsoft.Base.StiLicense.loadFromFile('stimulsoft.key')
 
     var report = new Stimulsoft.Report.StiReport();
     report.loadFile('Reports/SimpleList.mrt');
